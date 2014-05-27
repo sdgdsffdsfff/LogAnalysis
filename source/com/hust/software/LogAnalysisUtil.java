0a5b81a5a86f15a5dd64570d15163ab5e0b56009
yuyan2do <yuyan2do@gmail.com>
2014-05-27 17:07:09 +0800

    Add highlight for code inspection.     1)Highlight variables which need to be added to log.error;     2)Highlight method callers (method retrace);
    Add test code to generate control flow for method.

--- source/com/hust/software/LogAnalysisUtil.java
+++ source/com/hust/software/LogAnalysisUtil.java
@@ -1,15 +1,33 @@
 package com.hust.software;
 
+import com.intellij.codeInsight.highlighting.HighlightManager;
+import com.intellij.codeInspection.dataFlow.StandardDataFlowRunner;
+import com.intellij.codeInspection.dataFlow.StandardInstructionVisitor;
+import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.diagnostic.Logger;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.editor.colors.EditorColorsManager;
+import com.intellij.openapi.editor.markup.EffectType;
+import com.intellij.openapi.editor.markup.TextAttributes;
+import com.intellij.openapi.fileEditor.FileEditorManager;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.wm.StatusBar;
+import com.intellij.openapi.wm.WindowManager;
 import com.intellij.psi.*;
+import com.intellij.psi.controlFlow.*;
+import com.intellij.psi.search.searches.ReferencesSearch;
 import com.intellij.psi.util.PsiTreeUtil;
 import com.intellij.psi.util.TypeConversionUtil;
+import com.intellij.ui.JBColor;
+import org.jetbrains.annotations.NotNull;
 
+import java.awt.*;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * A tool to help analyse code for log enhance.
@@ -26,6 +44,7 @@ public class LogAnalysisUtil {
         if (logErrorCall == null || !checkLogFault(method, logErrorCall)) {
             return null;
         }
+        highlightMethodCaller(method);
         return logErrorCall;
     }
 
@@ -50,8 +69,62 @@ public class LogAnalysisUtil {
             LOG.info("Log error method call not found in " + method.getName() + ". ");
             return null;
         }
+//        while(true);
+        // todo these code is just for test
+        if (LOG.isDebugEnabled()) {
+            LOG.debug("### Instruction ###");
+            try {
+                ControlFlow controlFlow = ControlFlowFactory.getInstance(method.getProject())
+                        .getControlFlow(method.getBody(), LocalsOrMyInstanceFieldsControlFlowPolicy.getInstance(), true, true);
+                ControlFlowUtil.findCodeFragment(method);
+                controlFlow.getInstructions()
+                        .stream()
+                        .forEach((com.intellij.psi.controlFlow.Instruction ins) -> LOG.debug(ins.toString()));
+            } catch (AnalysisCanceledException e) {
+                e.printStackTrace();
+            }
+            LOG.debug("### end Instruction ###");
+
+//            new ControlFlowBuilder().build(dataFlowRunner, method.getBody());
+
+            LOG.debug("### Instruction2 ###");
+            StandardDataFlowRunner standardDataFlowRunner = new StandardDataFlowRunner(method.getBody());
+            standardDataFlowRunner.analyzeMethod(method.getBody(), new StandardInstructionVisitor());
+            com.intellij.codeInspection.dataFlow.instructions.Instruction[] instructions
+                    = standardDataFlowRunner.getInstructions();
+//            DfaValueFactory dfaValueFactory = standardDataFlowRunner.getFactory();
+//            Pair<Set<Instruction>, Set<Instruction>> constConditionalExpressions
+//                    = standardDataFlowRunner.getConstConditionalExpressions();
+
+            Stream.of(instructions)
+                    .forEach(instruction -> LOG.debug(instruction.getIndex() + "\t" + instruction.toString()));
+            LOG.debug("### end Instruction2 ###");
+        }
+
         LOG.info("Log error method call found in " + method.getName() + ". ");
         return logErrorCalls.get(0);
+    }
+
+    public static void highlightMethodCaller(PsiMethod method) {
+
+        /**
+         * search function call relationship
+         */
+        Collection<PsiReference> refMethodCaller = ReferencesSearch.search(method).findAll();
+        highlightElement(refMethodCaller
+                .stream()
+                .map(ref ->
+                        PsiTreeUtil.getParentOfType(ref.getElement(), PsiMethodCallExpression.class))
+                .collect(Collectors.toList()));
+
+        if (LOG.isDebugEnabled()) {
+            LOG.debug("### method call relationship ###");
+            refMethodCaller.stream()
+                    .map((PsiReference ref) -> ref.getElement().toString() + "\t" + ref.getElement().getParent())
+                    .forEach(LOG::debug);
+
+            LOG.debug("### end method call relationship ###");
+        }
     }
 
 
@@ -149,7 +222,52 @@ public class LogAnalysisUtil {
             refExpHaveAffectList.stream().map(Object::toString).forEach(LOG::debug);
             LOG.debug("### end ###");
         }
+        highlightElement(refExpNeedAddList.stream().map(PsiReference::getElement).collect(Collectors.toList()));
+
         return refExpNeedAddList;
+    }
+
+    private static void highlightElement(@NotNull List<PsiElement> elementList) {
+        if (elementList.size() == 0) {
+            return;
+        }
+        final PsiElement[] elements = elementList.toArray(new PsiElement[elementList.size()]);
+        final PsiElement element = elements[0];
+
+        ApplicationManager.getApplication().invokeLater(() ->
+                        ApplicationManager.getApplication().runWriteAction(() -> {
+                            final Project project = element.getProject();
+                            final FileEditorManager editorManager =
+                                    FileEditorManager.getInstance(project);
+                            final HighlightManager highlightManager =
+                                    HighlightManager.getInstance(project);
+                            final EditorColorsManager editorColorsManager =
+                                    EditorColorsManager.getInstance();
+                            final Editor editor = editorManager.getSelectedTextEditor();
+//                final EditorColorsScheme globalScheme =
+//                        editorColorsManager.getGlobalScheme();
+//        ColorKey.createColorKey()
+        /*final TextAttributes textattributes =
+                globalScheme.getAttributes(
+                        EditorColors.SEARCH_RESULT_ATTRIBUTES);
+                        */
+                            final TextAttributes textattributes =
+                                    new TextAttributes(null, JBColor.GREEN, null, EffectType.LINE_UNDERSCORE, Font.PLAIN);
+
+
+                            if (editor != null) {
+                                highlightManager.addOccurrenceHighlights(
+                                        editor, elements, textattributes, true, null);
+                                final WindowManager windowManager = WindowManager.getInstance();
+                                final StatusBar statusBar = windowManager.getStatusBar(project);
+                                if (statusBar != null) {
+                                    statusBar.setInfo("Press Esc to remove highlighting");
+                                }
+                            }
+
+                        })
+        );
+
     }
 
     private static boolean filterIfStatementByAssignInEitherBrench
